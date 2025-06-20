a76a89a85cec04de125bcbdb61bcfb50520cc85f
Kyoya67 <jackma8569@gmail.com>
2025-06-20 14:01:25 +0900

    feat: add cookies to understand server actions

--- packages/training-web-3/src/app/company-info/page.tsx
+++ packages/training-web-3/src/app/company-info/page.tsx
@@ -1,7 +1,23 @@
-export default function Page() {
+import { cookies } from "next/headers";
+
+async function setCookieAction() {
+  "use server";
+  const cookieStore = await cookies();
+  cookieStore.set("name", "John Doe");
+  console.log("server action", cookieStore.get("name"));
+}
+
+export default async function Page() {
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get("name");
+  console.log("page", cookie);
   return (
     <div>
       <h1>企業概要</h1>
+      <p>Cookie value: {cookie?.value || "No cookie found"}</p>
+      <form action={setCookieAction}>
+        <button type="submit">Set Cookie</button>
+      </form>
     </div>
   );
 }
